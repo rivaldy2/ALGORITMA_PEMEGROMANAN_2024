9bb8282f03097dc0351662bfe196255a52eb5f40
rivaldy2 <rivaldycoffee20@gmail.com>
2024-10-21 15:44:13 +0800

    Add files via upload

---
+++ Program_9.py
@@ -0,0 +1,26 @@
+# Program untuk melakukan operasi matematika dengan berbarapa tipe data
+# Interger
+a = 10
+b = 5
+hasil_int = a + b
+
+# Float
+x = 3.14
+y = 2.5
+hasil_float = x * y
+
+# String
+nama = "Jhon"
+umur = "25"
+hasil_string = "Nama: " + nama + ", Umur: " + umur
+
+# Boolean
+benar = True
+salah = False
+hasil_boolean = benar or salah
+
+# Menampilkan hasil operasi
+print("Hasil Interger:", hasil_int)
+print("Hasil Float:", hasil_float)
+print("Hasil String:", hasil_string)
+print("Hasil Boolean:", hasil_boolean)
